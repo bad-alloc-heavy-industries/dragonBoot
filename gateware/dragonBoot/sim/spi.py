254161439e041934bf4b800a5632f110c485948e
dragonmux <git@dragonmux.network>
2022-04-02 20:07:55 -0400

    Implemented SPI bus handling for the DFU handler

---
+++ gateware/dragonBoot/sim/spi.py
@@ -0,0 +1,78 @@
+from arachne.core.sim import sim_case
+from amaranth import Record
+from amaranth.hdl.rec import DIR_FANOUT, DIR_FANIN
+from amaranth.sim import Simulator, Settle
+
+from ..spi import SPIBus
+
+bus = Record((
+	('clk', [
+		('o0', 1, DIR_FANOUT),
+		('o1', 1, DIR_FANOUT),
+		('o_clk', 1, DIR_FANOUT),
+	]),
+	('cs', [
+		('o', 1, DIR_FANOUT),
+	]),
+	('copi', [
+		('o', 1, DIR_FANOUT),
+	]),
+	('cipo', [
+		('i', 1, DIR_FANIN),
+	]),
+))
+
+class Platform:
+	flashSize = 512 * 1024
+
+	def request(self, name, number, xdr = None):
+		assert name == 'flash'
+		assert number == 0
+		assert xdr is not None
+		assert xdr['clk'] == 2
+		return bus
+
+@sim_case(
+	domains = (('sync', 60e6),),
+	platform = Platform(),
+	dut = SPIBus(resource = ('flash', 0))
+)
+def spiBus(sim : Simulator, dut : SPIBus):
+	def sendRecv(dataOut, dataIn, overlap = False):
+		yield dut.w_data.eq(dataOut)
+		yield dut.xfer.eq(1)
+		yield Settle()
+		yield
+		yield dut.xfer.eq(0)
+		for bit in range(8):
+			yield bus.cipo.i.eq((dataIn >> (7 - bit)) & 1)
+			yield
+			yield Settle()
+			assert (yield bus.copi.o) == (dataOut >> (7 - bit)) & 1
+		assert (yield dut.done) == 1
+		if not overlap:
+			yield
+			yield Settle()
+			assert (yield dut.done) == 0
+			assert (yield dut.r_data) == dataIn
+
+	def domainSync():
+		yield
+		yield dut.cs.eq(1)
+		yield Settle()
+		yield
+		yield from sendRecv(0x0F, 0xF0)
+		yield
+		yield dut.cs.eq(0)
+		yield Settle()
+		yield
+		yield dut.cs.eq(1)
+		yield Settle()
+		yield
+		yield from sendRecv(0xAA, 0x55, overlap = True)
+		yield from sendRecv(0x55, 0xAA, overlap = False)
+		yield
+		yield dut.cs.eq(0)
+		yield Settle()
+		yield
+	yield domainSync, 'sync'
---
+++ gateware/dragonBoot/spi.py
@@ -0,0 +1,64 @@
+from amaranth import Elaboratable, Module, Signal, Cat, ClockSignal
+
+__all__ = (
+	'SPIBus',
+)
+
+class SPIBus(Elaboratable):
+	def __init__(self, *, resource):
+		self._spiResource = resource
+
+		self.cs = Signal()
+		self.xfer = Signal()
+		self.done = Signal()
+		self.r_data = Signal(8)
+		self.w_data = Signal(8)
+
+	def elaborate(self, platform):
+		m = Module()
+		bus = platform.request(*self._spiResource, xdr = {'clk': 2})
+
+		bit = Signal(range(8))
+		clkEn = Signal(1)
+		cipo = bus.cipo.i
+		copi = bus.copi.o
+
+		dataIn = Signal.like(self.r_data)
+		dataOut = Signal.like(self.w_data)
+
+		m.d.comb += [
+			clkEn.eq(0),
+			self.done.eq(0),
+		]
+
+		with m.FSM(name = 'spi'):
+			with m.State('IDLE'):
+				with m.If(self.xfer):
+					m.d.sync += dataOut.eq(self.w_data)
+					m.next = 'TRANSFER'
+			with m.State('TRANSFER'):
+				m.d.comb += clkEn.eq(1)
+				m.d.sync += [
+					bit.eq(bit + 1),
+					copi.eq(dataOut[7]),
+					dataOut.eq(dataOut.shift_left(1)),
+					dataIn.eq(Cat(cipo, dataIn[:-1])),
+				]
+				with m.If(bit == 7):
+					m.next = 'DONE'
+			with m.State('DONE'):
+				m.d.comb += self.done.eq(1)
+				m.d.sync += self.r_data.eq(dataIn)
+				with m.If(self.xfer):
+					m.d.sync += dataOut.eq(self.w_data)
+					m.next = 'TRANSFER'
+				with m.Else():
+					m.next = 'IDLE'
+
+		m.d.comb += [
+			bus.cs.o.eq(self.cs),
+			bus.clk.o0.eq(0),
+			bus.clk.o1.eq(clkEn),
+			bus.clk.o_clk.eq(ClockSignal('sync')),
+		]
+		return m
