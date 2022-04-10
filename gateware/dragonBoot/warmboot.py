905ea46138476aceef07d4240756b74ef0252fb3
dragonmux <git@dragonmux.network>
2022-04-10 16:23:36 -0400

    Begun implementing warmboot support for the iCE40 version of the bootloader

---
+++ gateware/dragonBoot/warmboot.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from amaranth import Elaboratable, Module, Signal, Instance
+from amaranth.vendor.lattice_ice40 import LatticeICE40Platform
+from amaranth.vendor.lattice_ecp5 import LatticeECP5Platform
+
+__all__ = (
+	'Warmboot',
+)
+
+class Warmboot(Elaboratable):
+	def __init__(self):
+		self.trigger = Signal()
+
+	def elaborate(self, platform):
+		m = Module()
+		if isinstance(platform, LatticeICE40Platform):
+			warmbootSelect = Signal(2)
+			m.d.comb += warmbootSelect.eq(0b01)
+
+			m.submodules += Instance(
+				'SB_WARMBOOT',
+				i_BOOT = self.trigger,
+				i_S0 = warmbootSelect[0],
+				i_S1 = warmbootSelect[1],
+			)
+		elif isinstance(platform, LatticeECP5Platform):
+			raise NotImplementedError("ECP5 support is planned but not yet implemented")
+		return m
