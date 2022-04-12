92c56340c91f64a456737b0af0d46ab12e96a2c9
dragonmux <git@dragonmux.network>
2022-04-12 06:47:21 -0400

    Implemented support into DFURequestHandler for alt-modes

--- gateware/dragonBoot/dfu.py
+++ gateware/dragonBoot/dfu.py
@@ -58,6 +58,7 @@ class DFURequestHandler(USBRequestHandler):
 		receiverDone = Signal()
 		receiverCount = Signal.like(setup.length)
 		receiverConsumed = Signal.like(setup.length)
+		slot = Signal(8)
 
 		_flash : Flash = platform.flash
 		config = DFUConfig()
@@ -109,6 +110,10 @@ class DFURequestHandler(USBRequestHandler):
 					with m.Elif(setup.type == USBRequestType.STANDARD):
 						# Switch to the right state for what we need to handle
 						with m.Switch(setup.request):
+							with m.Case(USBStandardRequests.GET_INTERFACE):
+								m.next = 'GET_INTERFACE'
+							with m.Case(USBStandardRequests.SET_INTERFACE):
+								m.next = 'SET_INTERFACE'
 							with m.Default():
 								m.next = 'UNHANDLED'
 					# If the underlying Flash operation is complete, signal this by going downloadSync
@@ -223,6 +228,39 @@ class DFURequestHandler(USBRequestHandler):
 				# ... and ACK our status stage.
 				with m.If(interface.status_requested):
 					m.d.comb += interface.handshakes_out.ack.eq(1)
+					m.next = 'IDLE'
+
+			# GET_INTERFACE -- The host is trying to ask what one of our interfaces' alt-mode is set to
+			with m.State('GET_INTERFACE'):
+				# Hook up the transmitter ...
+				m.d.comb += [
+					transmitter.stream.connect(interface.tx),
+					transmitter.max_length.eq(1),
+				]
+				m.d.comb += transmitter.data[0].eq(slot)
+
+				# ... then trigger it when requested if the lengths match ...
+				with m.If(self.interface.data_requested):
+					with m.If(setup.length == 1):
+						m.d.comb += transmitter.start.eq(1)
+					with m.Else():
+						m.d.comb += interface.handshakes_out.stall.eq(1)
+						m.next = 'IDLE'
+
+				# ... and ACK our status stage.
+				with m.If(interface.status_requested):
+					m.d.comb += interface.handshakes_out.ack.eq(1)
+					m.next = 'IDLE'
+
+			# SET_INTERFACE -- The host is trying to switch to one of our interface alt-modes
+			with m.State('SET_INTERFACE'):
+				# Provide a response to the status stage
+				with m.If(interface.status_requested):
+					m.d.comb += self.send_zlp()
+
+				# Copy the value once we get back an ACK from the ZLP
+				with m.If(interface.handshakes_in.ack):
+					m.d.usb += slot.eq(setup.value[0:8])
 					m.next = 'IDLE'
 
 			# UNHANDLED -- we've received a request we don't know how to handle
