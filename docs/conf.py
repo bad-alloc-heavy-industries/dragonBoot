e13afb1de5ab5b38d4a720172c618d3395fafbf4
dragonmux <git@dragonmux.network>
2022-04-21 22:53:17 -0400

    Configured a GitHub Action for building and deploying the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@ extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.intersphinx',
 	'sphinx.ext.todo',
+	'sphinx.ext.githubpages',
 	'sphinx.ext.graphviz',
 	'sphinx.ext.napoleon',
 	'sphinx_rtd_theme',
