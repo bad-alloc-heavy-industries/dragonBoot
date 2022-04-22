121fcf6afb7cc24a2c6c55231f1a7b5879b01eac
dragonmux <git@dragonmux.network>
2022-04-22 04:13:56 -0400

    Added documentation for the SPI engine gateware

--- gateware/dragonBoot/spi.py
+++ gateware/dragonBoot/spi.py
@@ -1,12 +1,37 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from amaranth import Elaboratable, Module, Signal, Cat
+from typing import Tuple
 
 __all__ = (
 	'SPIBus',
 )
 
 class SPIBus(Elaboratable):
-	def __init__(self, *, resource):
+	""" SPI bus controller gateware for talking to the Flash.
+
+	Attributes
+	--------
+	cs : Signal(), input
+		Chip Select (non-inverted) signal to be passed through to the Flash
+	xfer : Signal(), input
+		Strobe that signals to start a SPI transfer
+	done : Signal(), output
+		Strobe that indicates the completion of a SPI transfer
+
+	r_data : Signal(8), output
+		Data read from the Flash in the last completed transfer
+	w_data : Signal(8), input
+		Data to be written to the Flash in the next transfer
+	"""
+
+	def __init__(self, *, resource : Tuple[str, int]):
+		"""
+		Parameters
+		----------
+		resource
+			The fully qualified identifier for the platform resource defining the SPI bus to use
+		"""
+
 		self._spiResource = resource
 
 		self.cs = Signal()
@@ -15,7 +40,19 @@ class SPIBus(Elaboratable):
 		self.r_data = Signal(8)
 		self.w_data = Signal(8)
 
-	def elaborate(self, platform):
+	def elaborate(self, platform) -> Module:
+		""" Describe the specific gateware needed to talk SPI protocol
+
+		Parameters
+		----------
+		platform
+			The Amaranth platform for which the gateware will be synthesised
+
+		Returns
+		-------
+		amaranth.hdl.dsl.Module
+			A complete description of the gateware behaviour required
+		"""
 		m = Module()
 		bus = platform.request(*self._spiResource)
 
