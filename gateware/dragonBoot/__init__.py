eacb4e65b4a23c3f5b3c20dc1ed1b7f55a683156
dragonmux <git@dragonmux.network>
2023-10-19 04:35:57 +0100

    gateware: Refreshed the CLI to drop the dependency on Arachne

--- gateware/dragonBoot/__init__.py
+++ gateware/dragonBoot/__init__.py
@@ -12,19 +12,29 @@ __all__ = (
 
 def cli():
 	from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-	from arachne.cli import register_cli
 
+	# Build the command line parser
 	parser = ArgumentParser(formatter_class = ArgumentDefaultsHelpFormatter,
 		description = 'dragonBoot')
+	parser.add_argument('--verbose', '-v', action = 'store_true', help = 'Enable debugging output')
+
+	# Create action subparsers for building and simulation
 	actions = parser.add_subparsers(dest = 'action', required = True)
 	buildAction = actions.add_parser('build', help = 'build the dragonBoot DFU gateware')
+	actions.add_parser('sim', help = 'Simulate and test the gateware components')
+
+	# Populate the possible build targets
 	platforms = listPlatforms()
 	buildAction.add_argument('--target', action = 'store', required = True, choices = platforms.keys())
 
-	register_cli(parser = parser)
+	# Parse the command line and, if `-v` is specified, bump the logging level
 	args = parser.parse_args()
+	if args.verbose:
+		from logging import root, DEBUG
+		root.setLevel(DEBUG)
 
-	if args.action == 'arachne-sim':
+	# Dispatch the action requested
+	if args.action == 'sim':
 		from .sim.framework import run_sims
 		run_sims(pkg = 'dragonBoot/sim', result_dir = 'build')
 		return 0
